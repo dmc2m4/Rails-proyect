9d9c8893a40fef15c07c4badf584fe85c41bb755
dmc2m4 <danielmartinezcabrera@hotmail.com>
2023-03-21 16:01:21 -0500

    login user created

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,13 @@
 class AuthenticationController < ApplicationController
+
+    def login
+        @user = User.find_user_by_email(params[:email])
+        if @user&.authenticate(params[:password_digest])
+            token = JsonWebToken.jwt_encode(user_id: @user.id)
+            render json: {token: token}, status: :ok
+        else
+            render json: { error: 'Invalid username or password' }, status: :unauthorized
+        end
+    end
+
 end
