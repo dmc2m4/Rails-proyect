d98bf096242d4b634d681811624dc64c5d1c4752
dmc2m4 <danielmartinezcabrera@hotmail.com>
2023-03-21 15:15:43 -0500

    user controller created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json: @users, status: :ok
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user, status: :ok
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save 
+            render json: @user, status: :ok
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
 end
