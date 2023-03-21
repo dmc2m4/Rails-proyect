a513ccff088be173edef5da17dbcdfe0c3a0e42f
dmc2m4 <danielmartinezcabrera@hotmail.com>
2023-03-21 15:08:08 -0500

    user validations created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+    require "securerandom"
+    has_secure_password
+
+    validates :email, presence: true, unique: true
+    validates :password, presence: true, length: { minimum: 6 }
+    validates :username, presence: true, unique: true
+    validates :name, presence: true
+
 end
