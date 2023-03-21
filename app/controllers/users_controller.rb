310f37eb36205f3109837f9ec86053e4d020ad61
dmc2m4 <danielmartinezcabrera@hotmail.com>
2023-03-21 15:20:53 -0500

    CRUD for user created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,15 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        head :no_content
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :img, :password_digest, :name)
+
 end
